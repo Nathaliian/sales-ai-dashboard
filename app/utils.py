71344c87b3b0a215d0d72bbe14d1724cfd01bfc5
Nathalian <nathvapaks@gmail.com>
2026-04-21 12:04:23 +0530

    remove sqlalchemy and db completely

--- app/utils.py
+++ app/utils.py
@@ -1,14 +1,40 @@
 import pandas as pd
-from app.db import engine
+import os
+import re
 
-def run_query(sql):
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+file_path = os.path.join(BASE_DIR, "data", "store_sales_data_cleaned.csv")
+
+df = pd.read_csv(file_path)
+
+
+def run_query(sql: str):
     try:
-        # basic safety check
-        if "delete" in sql.lower():
-            return "Dangerous query blocked"
+        sql = sql.lower()
 
-        df = pd.read_sql(sql, engine)
-        return df
+        # TOTAL SALES
+        if "sum(sales)" in sql and "group by" not in sql:
+            total = df["sales"].sum()
+            return pd.DataFrame([{"total_sales": total}])
+
+        # GROUP BY
+        match = re.search(r"group by ([a-z_]+)", sql)
+        if match:
+            col = match.group(1)
+
+            if col in df.columns:
+                result = df.groupby(col)["sales"].sum().reset_index()
+                result.columns = [col, "total_sales"]
+                return result
+
+        # TOP N
+        if "top" in sql:
+            match = re.search(r"top (\d+)", sql)
+            if match:
+                n = int(match.group(1))
+                return df.nlargest(n, "sales")[["product_name", "sales"]]
+
+        return df.head(10)
 
     except Exception as e:
         return str(e)
